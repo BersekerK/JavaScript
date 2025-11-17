4d277be9256459fbe21fc37c3cfbd8ab4eb86bd6
BersekerK <legakagod@gmail.com>
2025-11-17 19:42:42 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+      let num1 = 5;
+      let num2 = 10;
+      console.log( "Il primo numero è: " + num1 );
+      console.log( "Il secondo numero è: " + num2 );
+
+      let stringa = "stringa";
+      stringa = "nuova stringa";
+      console.log( `Questa invece è una ${stringa}` );
+
+
+        const pGreco = 3.14;
+        console.log( `Il valore di pGreco è: ${pGreco}` );
+
+        pGreco = 659;
+        console.log( `Il nuovo valore di pGreco è: ${pGreco}` );
