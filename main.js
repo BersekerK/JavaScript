f46bf0d8a88b2713bde69a2f00c8b9c37435a8c8
BersekerK <legakagod@gmail.com>
2025-12-09 19:42:23 +0100

    Condizioni1

--- main.js
+++ main.js
@@ -1,15 +1,60 @@
-      let num1 = 5;
-      let num2 = 10;
-      console.log( "Il primo numero è: " + num1 );
-      console.log( "Il secondo numero è: " + num2 );
+console.log("--- SOLUZIONE CON IF/ELSE ---");
 
-      let stringa = "stringa";
-      stringa = "nuova stringa";
-      console.log( `Questa invece è una ${stringa}` );
+function convertVotoIfElse(v) {
+  if (v < 18) {
+    console.log("insufficiente");
+  } else if (v >= 18 && v < 21) {
+    console.log("sufficiente");
+  } else if (v >= 21 && v < 24) {
+    console.log("buono");
+  } else if (v >= 24 && v < 27) {
+    console.log("distinto");
+  } else if (v >= 27 && v <= 29) {
+    console.log("ottimo");
+  } else if (v === 30) {
+    console.log("eccellente");
+  }
+}
+
+let v = 29;
+console.log("Voto: " + v);
+convertVotoIfElse(v);
+
+console.log("\n--- SOLUZIONE CON SWITCH ---");
 
 
-        const pGreco = 3.14159265359;
-        console.log( `Il valore di pGreco è: ${pGreco}` );
+function convertVotoSwitch(voto) {
+  switch (true) {
+    case voto < 18:
+      console.log("insufficiente");
+      break;
+    case voto >= 18 && voto < 21:
+      console.log("sufficiente");
+      break;
+    case voto >= 21 && voto < 24:
+      console.log("buono");
+      break;
+    case voto >= 24 && voto < 27:
+      console.log("distinto");
+      break;
+    case voto >= 27 && voto <= 29:
+      console.log("ottimo");
+      break;
+    case voto === 30:
+      console.log("eccellente");
+      break;
+  }
+}
 
-        pGreco = 659;
-        console.log( `Il nuovo valore di pGreco è: ${pGreco}` );
+
+console.log("\nRisultati con IF/ELSE:");
+votiTest.forEach(voto => {
+  process.stdout.write("Voto " + voto + ": ");
+  convertVotoIfElse(voto);
+});
+
+console.log("\nRisultati con SWITCH:");
+votiTest.forEach(voto => {
+  process.stdout.write("Voto " + voto + ": ");
+  convertVotoSwitch(voto);
+});
