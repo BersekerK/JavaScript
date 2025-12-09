3b2630d0746ab1a50b094deadb172114646cc8c0
BersekerK <legakagod@gmail.com>
2025-12-09 20:42:22 +0100

    cicli_1

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
-      let num1 = 5;
-      let num2 = 10;
-      console.log( "Il primo numero è: " + num1 );
-      console.log( "Il secondo numero è: " + num2 );
+// Tabellina del 2
+let num = 2;
+for (let i = 1; i <= 10; i++) {
+	console.log(`${num} x ${i} = ${num * i}`);
+}
 
-      let stringa = "stringa";
-      stringa = "nuova stringa";
-      console.log( `Questa invece è una ${stringa}` );
-
-
-        const pGreco = 3.14159265359;
-        console.log( `Il valore di pGreco è: ${pGreco}` );
-
-        pGreco = 659;
-        console.log( `Il nuovo valore di pGreco è: ${pGreco}` );
