2794189f962b6ddefdffe385fa8ba0eba374a7c6
BersekerK <legakagod@gmail.com>
2025-12-16 17:35:22 +0100

    giocodadi
    Ho aggiunto anche la selezione nomi giocatori

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-      let num1 = 5;
-      let num2 = 10;
-      console.log( "Il primo numero è: " + num1 );
-      console.log( "Il secondo numero è: " + num2 );
+function giocoDeiDadi() {
+    let giocatore1 = prompt("Inserisci il nome del primo giocatore 1: ");
+    let giocatore2 = prompt("Inserisci il nome del secondo giocatore 2: ");
+    let n = prompt("Quanti tiri volete fare?");
+    let punteggio1 = 0;
+    for (let i = 0; i < n; i++) {
+        let lancio = Math.floor(Math.random() * 6) + 1;
+        punteggio1 += lancio;
+    }
 
-      let stringa = "stringa";
-      stringa = "nuova stringa";
-      console.log( `Questa invece è una ${stringa}` );
+    let punteggio2 = 0;
+    for (let i = 0; i < n; i++) {
+        let lancio = Math.floor(Math.random() * 6) + 1;
+        punteggio2 += lancio;
+    }
 
+    console.log(`Punteggio ${giocatore1}: ` + punteggio1);
+    console.log(`Punteggio ${giocatore2}: ` + punteggio2);
 
-        const pGreco = 3.14159265359;
-        console.log( `Il valore di pGreco è: ${pGreco}` );
-
-        pGreco = 659;
-        console.log( `Il nuovo valore di pGreco è: ${pGreco}` );
+    if (punteggio1 > punteggio2) {
+        console.log(`Il vincitore è ${giocatore1}!`);
+    } else if (punteggio2 > punteggio1) {
+        console.log(`Il vincitore è ${giocatore2}!`);
+    } else {
+        console.log("Pareggio!");
+    }
+}
+giocoDeiDadi();
