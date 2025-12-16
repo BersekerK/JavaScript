9f1b4a728b57c48f824825f8dce0a6f654096832
BersekerK <legakagod@gmail.com>
2025-12-16 16:50:14 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-      let num1 = 5;
-      let num2 = 10;
-      console.log( "Il primo numero è: " + num1 );
-      console.log( "Il secondo numero è: " + num2 );
+let array_1 = [
+  ['un', 'per', 'incatenarli.'],
+  ['Anello', 'trovarli,'],
+  ['ghermirli', 'e'],
+  ['gondor', 'mark'],
+];
 
-      let stringa = "stringa";
-      stringa = "nuova stringa";
-      console.log( `Questa invece è una ${stringa}` );
+let array_2 = [
+  [['trovarli,']],
+  ['tu,', 'sciocchi'],
+  ['tu,', 'sciocchi', ['padron', 'Sauron']],
+  ['nel', ['fuggite', 'gandalf']],
+  [['domarli,', 'passare'], 'buio']
+];
 
-
-        const pGreco = 3.14159265359;
-        console.log( `Il valore di pGreco è: ${pGreco}` );
-
-        pGreco = 659;
-        console.log( `Il nuovo valore di pGreco è: ${pGreco}` );
+console.log(array_1[0][0], array_1[1][0], array_1[0][1], array_2[4][0][0], array_1[0][0], array_1[1][0], array_1[0][1], array_1[1][1], array_1[0][0], array_1[1][0], array_1[0][1], array_1[2][0], array_1[2][1], array_2[3][0], array_2[4][1], array_1[0][2]);
